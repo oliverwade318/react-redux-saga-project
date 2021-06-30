2091c190604826d49e4b0cbe35f736c8fe33d45d
John <johnyei.business@gmail.com>
2021-06-29 21:44:13 -0300

    registrate new users in the application

--- src/pages/Login/style.js
+++ src/pages/Login/style.js
@@ -1,9 +1,32 @@
 import styled from "styled-components";
+import * as colors from "../../config/colors";
 
 export const Title = styled.h1`
-  color: ${(props) => (props.isRed ? "red" : "blue")};
+  color: ${(props) => (props.isBlack ? "black" : "red")};
+  text-align: center;
 `;
 
-export const Paragraph = styled.p`
-  font-family: sans-serif;
+export const Form = styled.form`
+  input {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 10px;
+    margin: 15px 0;
+    border-radius: 5px;
+
+    &:focus {
+      border: 1px solid ${colors.PrimaryColor};
+    }
+  }
+
+  button {
+    width: 100%;
+    background-color: ${colors.PrimaryColor};
+    transition: all 300ms;
+
+    &:hover {
+      filter: brightness(80%);
+    }
+  }
 `;
